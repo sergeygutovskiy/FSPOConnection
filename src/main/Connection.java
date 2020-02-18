d72f81d6cf4942f30a878b740d47cb74121e0b02
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-02-18 18:06:16 +0300

    added student's lessons request

--- src/main/Connection.java
+++ src/main/Connection.java
@@ -6,6 +6,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+
 public class Connection {
 	
 	public static String login(String url, String name, String password) throws IOException, InterruptedException {
@@ -20,13 +24,33 @@ public class Connection {
 
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
- 
-        String cookie = response.headers().map().get("set-cookie").get(4);			
+        
+        Integer response_cookies_count = response.headers().map().get("set-cookie").size();
+        String cookie = new String(response.headers().map().get("set-cookie").get(response_cookies_count - 1));			
 		return cookie;
 	}
 	
-	public static String getStudentPage(String url, String cookie) throws IOException, InterruptedException {
+//	public static String getStudentPage(String url, String cookie) throws IOException, InterruptedException {
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create(url))
+//                .setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+//                .setHeader("Cookie", cookie)
+//                .setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.95")
+//                .build();
+//
+//        HttpResponse<String> response = client.send(request,
+//                HttpResponse.BodyHandlers.ofString());
+//
+//        String responseBody = response.body();
+//        return responseBody;
+//	}
+	
+	public static JSONObject getStudentLessons(String url, String student_id, Integer year, String month, String cookie) throws IOException, InterruptedException, ParseException {
         HttpClient client = HttpClient.newHttpClient();
+        
+        url += "?stud=" + student_id + "&dateyear=" + year + "&datemonth=" + month;
+        
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
@@ -37,7 +61,9 @@ public class Connection {
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
 
-        String responseBody = response.body();
-        return responseBody;
+        Object obj = new JSONParser().parse(response.body());
+        JSONObject jo = (JSONObject) obj;
+        
+        return jo;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,49 @@
 package main;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Iterator;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
 import main.Connection;
 
+
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException{
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException{
     	String cookie = Connection.login(
     			"https://ifspo.ifmo.ru", 
     			"gutovskijsergejaleksandrovich", 
     			"pbaah8kc");
-    	String body = Connection.getStudentPage("https://ifspo.ifmo.ru/profile", cookie);
     	
-    	String buffer = "";
-    	for (int i = 1; i < body.length(); i++) {
-    		if (!(body.charAt(i) == ' ' && body.charAt(i - 1) == ' ')) {
-    			buffer += body.charAt(i);
-    		}
+//    	String body = Connection.getStudentPage("https://ifspo.ifmo.ru/profile", cookie);
+
+    	String[] decoded_cookie = URLDecoder.decode(cookie).toString().split("s:");
+    	String userIdDirty = decoded_cookie[decoded_cookie.length - 5].split(":")[1];
+    	String userId = userIdDirty.substring(1, userIdDirty.length() - 2);
+//    	System.out.println(user_id);
+    	
+    	Integer currentYear = LocalDate.now().getYear();
+    	String currentMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("MM"));
+    	
+    	JSONObject lessonsData;
+    	lessonsData = Connection.getStudentLessons("https://ifspo.ifmo.ru/profile/getStudentLessonsVisits", 
+    			userId, 
+    			currentYear, 
+    			currentMonth, 
+    			cookie);
+
+    	JSONArray lessons = (JSONArray) lessonsData.get("userlessons");
+    	
+    	Iterator lessonsIterator = (Iterator) lessons.iterator();
+    	while (lessonsIterator.hasNext()) {
+    		JSONObject lesson = (JSONObject) lessonsIterator.next();
+    		System.out.println("Name: " + lesson.get("name"));
+    		System.out.println("Semester: " + lesson.get("semester"));
+    		System.out.println("ID: " + lesson.get("id"));
     	}
-    	
-    	System.out.println(body);
     }
 }
